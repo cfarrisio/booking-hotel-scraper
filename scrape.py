48708a97757d233beef3af7deedc71d974d99f31
cfarrisio <126063406+cfarrisio@users.noreply.github.com>
2023-06-04 06:29:55 -0400

    update

--- scrape.py
+++ scrape.py
@@ -2,6 +2,7 @@ from selectorlib import Extractor
 import requests 
 from time import sleep
 import csv
+import random
 
 # Create an Extractor by reading from the YAML file
 e = Extractor.from_yaml_file('booking.yml')
@@ -13,10 +14,8 @@ def scrape(url):
         'Cache-Control': 'no-cache',
         'DNT': '1',
         'Upgrade-Insecure-Requests': '1',
-        # You may want to change the user agent if you get blocked
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-
         'Referer': 'https://www.booking.com/index.en-gb.html',
         'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
@@ -27,7 +26,6 @@ def scrape(url):
     # Pass the HTML of the page and create 
     return e.extract(r.text,base_url=url)
 
-# product_data = []
 with open("urls.txt",'r') as urllist, open('data.csv','w') as outfile:
     fieldnames = [
         "name",
@@ -44,9 +42,8 @@ with open("urls.txt",'r') as urllist, open('data.csv','w') as outfile:
     writer = csv.DictWriter(outfile, fieldnames=fieldnames,quoting=csv.QUOTE_ALL)
     writer.writeheader()
     for url in urllist.readlines():
-        data = scrape(url) 
-        if data:
+        data = scrape(url.strip()) 
+        if data and 'hotels' in data:
             for h in data['hotels']:
                 writer.writerow(h)
-            # sleep(5)
-    
+            sleep(random.uniform(3, 5))  # Use random sleep between 3 and 5 seconds
